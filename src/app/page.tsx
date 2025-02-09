0f869b36602e55327409de1e2e5d26b01beb8512
javimogan <diazalonsojavi@gmail.com>
2025-02-09 19:04:31 +0100

    Color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,6 +44,8 @@ function flow2Timers(flow: IBlock['blocks']) {
 
 export default function Home() {
   //Timer de 10 minutos
+  const [realStartTime, setRealStartTime] = React.useState<string | undefined>();
+  const [realStopTime, setRealStopTime] = React.useState<string | undefined>();
   const [block, setBlock] = React.useState<IBlock>(blockMorning);
   const [timers, setTimers] = React.useState<ITime[]>([]);
   const [currentTimer, setCurrentTimer] = React.useState<number>(0);
@@ -58,7 +60,7 @@ export default function Home() {
     setIsCounting(false);
     timer.current?.reset();
   }, [block]);
-  const [startBlock, endBlock] = React.useMemo(() => {
+  const [startBlock, endBlock, stepsBlock] = React.useMemo(() => {
     const timers = flow2Timers(block.blocks);
     const _start = moment(block.start, 'HH:mm');
     const _end = _start.clone();
@@ -85,13 +87,19 @@ export default function Home() {
 
     <main className="flex flex-col items-center my-8 gap-4">
       <section className="flex flex-row items-center gap-2">
-        <button onClick={() => setBlock(blockMorning)} type="button" className={`text-white bg-${block.name==='Morning'?'slate':'gray'}-800 hover:bg-${block.name==='Morning'?'slate':'gray'}-900  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2`}>{blockMorning.name}</button>
-        <button onClick={() => setBlock(blockAfternoon)} type="button" className={`text-white bg-${block.name==='Afternoon'?'slate':'gray'}-800 hover:bg-${block.name==='Afternoon'?'slate':'gray'}-900  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2`}>{blockAfternoon.name}</button>
+        <button onClick={() => setBlock(blockMorning)} type="button" className={`text-white ${block.name === 'Morning' ? 'bg-amber-800 hover:bg-amber-900' : 'bg-gray-800 hover:bg-gray-900'}  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2`}>{blockMorning.name}</button>
+        <button onClick={() => setBlock(blockAfternoon)} type="button" className={`text-white ${block.name === 'Afternoon' ? 'bg-amber-800 hover:bg-amber-900' : 'bg-gray-800 hover:bg-gray-900'}  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2`}>{blockAfternoon.name}</button>
       </section>
       <audio ref={audioRef} src="/audio/sound_1.mp3" />
       <div className="flex flex-row items-center gap-2">
         <span className='text-1xl font-bold'>{block.name}</span>
         <span>{startBlock} ~ {endBlock}</span>
+        {realStartTime && realStopTime && (
+          <>
+            <span>//</span>
+            <span>{realStartTime} ~ {realStopTime}</span>
+          </>
+        )}
       </div>
       <ProgressBalls currentIndex={currentTimer} balls={timers.map((t) => t.color)} />
       <PomodoroTimer
@@ -108,7 +116,14 @@ export default function Home() {
           (
             <button
               className="text-white bg-green-800 hover:bg-green-900  font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
-              onClick={() => { setIsCounting(true) }}>
+              onClick={() => {
+                if (!realStartTime) {
+                  setRealStartTime(moment().format('HH:mm'));
+                }
+                const totalDuration = timers.reduce((acc, t) => acc + t.duration, 0);
+                setRealStopTime(moment().add(totalDuration, 'seconds').format('HH:mm'));
+                setIsCounting(true)
+              }}>
               Start
             </button>
           )
