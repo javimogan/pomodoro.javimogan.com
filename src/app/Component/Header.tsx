db76c4963e47e9cc43d972d71a434d1845397336
javimogan <diazalonsojavi@gmail.com>
2025-02-11 20:58:42 +0100

    Easter egg

--- src/app/Component/Header.tsx
+++ src/app/Component/Header.tsx
@@ -13,7 +13,11 @@ export default function Header() {
             <div className='flex items-center justify-center gap-2'>
                 <img src='/images/pomodoro.svg' width={40} alt='Tomato' className='inline-block' />
                 <h1 className='text-4xl'>
+                    <a
+                    rel='noindex noreferrer nofollow '
+                    href='https://www.youtube.com/watch?v=6dXWetS91_Y' className='text-black'>
                     POMODORO
+                    </a>
                 </h1>
                 {process.env.NODE_ENV === 'development' && (
                     <span className='text-xs bg-red-500 text-white rounded-md p-1'>
